3e5ed986ca60a55c469c1bd36f17b8838005e13b
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-03 16:18:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,8 @@ end
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {railroads: {
-    pieces: 4, 
-    names: {}, 
+    pieces: 4,
+    names: {},
     rent_in_dollars: {}
     }
   }
@@ -23,13 +23,13 @@ end
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
   monopoly = {railroads: {
-      pieces: 4, 
+      pieces: 4,
       names: {
         reading_railroad: {},
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
         shortline: {}
-      }, 
+      },
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
@@ -43,13 +43,13 @@ end
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
   monopoly = {railroads: {
-      pieces: 4, 
+      pieces: 4,
       names: {
         reading_railroad: {"mortgage_value" => "$100"},
         pennsylvania_railroad: {"mortgage_value" => "$200"},
         b_and_o_railroad: {"mortgage_value" => "$400"},
         shortline: {"mortgage_value" => "$800"}
-      }, 
+      },
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
